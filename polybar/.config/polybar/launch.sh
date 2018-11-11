1f3c316d3ab47bfc0cb51446e5346ab8ea68ef74
Gerry Agbobada <gagbobada+git@gmail.com>
2018-11-11 16:32:03 +0100

    Polybar : make a top and a bottom bar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,6 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch mybar
-polybar -r mybar &
+polybar -r top &
+polybar -r bottom &
 
 echo "Bars launched..."
